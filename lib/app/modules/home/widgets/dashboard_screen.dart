4aff5c66697854b5daf344dd66acea415ead4d22
FaytMx <silverzero55@gmail.com>
2021-05-14 15:55:06 -0500

    diseño parte 3

--- lib/app/modules/home/widgets/dashboard_screen.dart
+++ lib/app/modules/home/widgets/dashboard_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:globalpaq_app/app/modules/home/models/RecentFile.dart';
 import 'package:globalpaq_app/app/modules/home/widgets/header.dart';
 import 'package:globalpaq_app/app/modules/home/widgets/my_fiels.dart';
+import 'package:globalpaq_app/app/modules/home/widgets/recent_files.dart';
 import 'package:globalpaq_app/app/modules/home/widgets/storage_details.dart';
 import 'package:globalpaq_app/app/utils/constatnts.dart';
 
@@ -29,51 +28,7 @@ class DashboardScreen extends StatelessWidget {
                       SizedBox(
                         height: defaultPadding,
                       ),
-                      Container(
-                        padding: EdgeInsets.all(defaultPadding),
-                        decoration: BoxDecoration(
-                            color: secondaryColor,
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10))),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Recent Files",
-                              style: Theme.of(context).textTheme.subtitle1,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              child: DataTable(
-                                columns: [
-                                  DataColumn(label: Text("File Name")),
-                                  DataColumn(label: Text("Date")),
-                                  DataColumn(label: Text("Size")),
-                                ],
-                                rows: [
-                                  DataRow(
-                                    cells: [
-                                      DataCell(
-                                        Row(
-                                          children: [
-                                            SvgPicture.asset(demoRecentFiles[0].icon, height: 30, width: 30,),
-                                            Padding(
-                                              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-                                              child: Text(demoRecentFiles[0].title),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      DataCell(Text("Text")),
-                                      DataCell(Text("Text")),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      RecentFiles(),
                     ],
                   ),
                 ),
